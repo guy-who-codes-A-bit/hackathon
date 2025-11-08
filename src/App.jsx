05b0b3c51108fbb332cb116826ebc6997eca0366
Andrew Tan <codedreww@gmail.com>
2025-11-08 13:59:21 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-green-300 text-4xl font-bold">
+      Tailwind is working! 🌿
+    </div>
+  );
+}
+
+export default App;
