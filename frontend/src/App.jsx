685e6e8fd94ea64c79839b2cb2cd3ceef587fa59
Andrew Tan <codedreww@gmail.com>
2025-11-08 23:38:10 -0700

    Removed AIBox and old dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
 import Landing from "./pages/Landing";
 import Signup from "./pages/Signup";
 import QRPage from "./pages/QRPage";
@@ -18,7 +17,6 @@ function App() {
       <div className="min-h-screen bg-green-300">
         <Routes>
           <Route path="/" element={<Login />} />
-          <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/landing" element={<Landing />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/qrpage" element={<QRPage />} />
